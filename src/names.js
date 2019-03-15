c18036f3fd4e9c8598104600ad0aa7641c2aa27e
nipunbatra1804 <nipunbatra1994@gmail.com>
2019-03-15 23:01:08 +0800

    update namelist

--- src/names.js
+++ src/names.js
@@ -1,14 +1,24 @@
 const names = [
-  "Hui Tian",
-  "Pi Wei",
-  "Ying Qi",
-  "Sabrina",
-  "Nicole",
-  "Pakata",
-  "Nipun",
-  "Jerome",
-  "Nicholas",
-  "Timothy"
+  "TAN CHIA CHUN",
+  "TAN JIA MIN",
+  "TAN WEI HUA",
+  "WANG WENWEI",
+  "WANG YIXIAN",
+  "WANG YUWEI",
+  "DARREN",
+  "WEI YAQIN",
+  "WONG MAYNARD",
+  "ALOYSIUS",
+  "WU QIYANG",
+  "XU XIN",
+  "XU ZIPENG",
+  "YANG SHUOGENG",
+  "YU HUI",
+  "ZARIFAH BINTE ZULKIFLI",
+  "ZHANG MENGTONG",
+  "ZHANG YUN",
+  "ZHAO WENDI",
+  "ZHAO YUECHEN"
 ];
 
 export default names;
